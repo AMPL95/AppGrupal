ddb8376876373f413334f7986807641e2622971a
Louis <crack2380@gmail.com>
2022-02-07 19:09:08 +0100

    Inicias sesión y te envía un mensaje al email para cambiar la contraseña

--- app/src/main/java/com/example/appgrupal/PasswordReset.kt
+++ app/src/main/java/com/example/appgrupal/PasswordReset.kt
@@ -1,24 +1,55 @@
 package com.example.appgrupal
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.reset_password.*
 
+
+
 class PasswordReset : AppCompatActivity() {
-    private val db = FirebaseFirestore.getInstance()
+    private var mAuth: FirebaseAuth? = null
+    /*private val db = FirebaseFirestore.getInstance()*/
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.reset_password)
+        mAuth = FirebaseAuth.getInstance()
 
-        val miboton: Button = findViewById(R.id.botonReset)
+       botonReset.setOnClickListener {
+            val email = emailReset.text.toString().trim()
+
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(applicationContext, "Enter your email!", Toast.LENGTH_SHORT).show()
+            } else {
+                mAuth!!.sendPasswordResetEmail(email)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this@PasswordReset, "Check email to reset your password!", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(this@PasswordReset, "Fail to send reset password email!", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+        }
+
+        btnBack.setOnClickListener {
+            finish()
+        }
+        //Para los email que no tenemos guardo la contraseña
+       /* val miboton: Button = findViewById(R.id.botonReset)
         miboton.setOnClickListener{
 
             db.collection("clientes").document(emailReset.text.toString()).get().addOnSuccessListener {
                 recuperarpassw.setText(it.get("Passwd") as String?)
+
+
             }
-        }
+        }*/
     }
 
 }
