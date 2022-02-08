c907f080637be886fb55d20a3636fdd53699cd68
Sara <saraberrenderomoya@gmail.com>
2022-02-08 13:00:47 +0100

    activity informes

---
+++ app/src/main/java/com/example/appgrupal/informes.kt
@@ -0,0 +1,32 @@
+package com.example.appgrupal
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import com.google.firebase.storage.FirebaseStorage
+
+
+class informes : AppCompatActivity() {
+
+    private val storage = FirebaseStorage.getInstance()
+    private val storageRef = storage.getReferenceFromUrl("gs://appgrupal-a11b0.appspot.com/pdf/prueba.pdf")
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_informes)
+
+        val miboton: ImageView = findViewById(R.id.imagepdf)
+        miboton.setOnClickListener {
+
+            storageRef.child("pdf/prueba.pdf").downloadUrl.addOnSuccessListener {
+
+            }.addOnFailureListener {
+
+            }
+
+
+        }
+
+
+    }
+}
