046169825cf6aabcbfb66509be2669696e2b2779
Vitycani <jmspedre@hotmail.com>
2022-02-13 23:12:12 +0100

    Commit Videollamada

---
+++ app/src/main/java/com/example/appgrupal/Videollamada/VideoLLamada.kt
@@ -0,0 +1,104 @@
+package com.example.appgrupal.Videollamada
+
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.media.tv.TvContract
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.appgrupal.R
+import com.example.appgrupal.Videollamada.Constants.TAG
+import com.example.appgrupal.databinding.ActivityVideollamadaBinding
+import kotlin.math.log
+
+class VideoLLamada: AppCompatActivity() {
+
+    private lateinit var binding: ActivityVideollamadaBinding
+
+    private var imageCapture: ImageCapture?=null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityVideollamadaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (allPermissionGranted()){
+            startCamera()
+        }else{
+            ActivityCompat.requestPermissions(
+                this, Constants.REQUIRED_PERMISSIONS, Constants.REQUEST_CODE_PERMISSIONS
+            )
+        }
+    }
+
+    private fun startCamera(){
+
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+
+            val  cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder().build().also {
+                mPreview->
+                mPreview.setSurfaceProvider(
+                    binding.viewFinder.surfaceProvider
+                )
+            }
+
+        imageCapture= ImageCapture.Builder().build()
+
+            val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+
+            try {
+
+                cameraProvider.unbindAll()
+
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector,preview,imageCapture
+                )
+
+            }catch (e:Exception){
+                Log.d(Constants.TAG, "Error al encender la camara", e)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        //
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == Constants.REQUEST_CODE_PERMISSIONS){
+            if (allPermissionGranted()){
+                startCamera()
+            }else{
+                Toast.makeText(this, "El usuario no ha otrogado los permisos", Toast.LENGTH_SHORT).show()
+
+            finish()
+            }
+        }
+    }
+
+
+    private fun allPermissionGranted()=
+        Constants.REQUIRED_PERMISSIONS.all {
+            ContextCompat.checkSelfPermission(
+                baseContext, it
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+
+}
